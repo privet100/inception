e43f6252f62a70c6d427f82d1bce6dbc86fd3d60
Anna <internet124816@gmail.com>
2024-06-30 22:55:26 +0200

    Delete project/srcs/requirements/wordpress/tools/make_dir.sh

--- project/srcs/requirements/wordpress/tools/make_dir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# проверить на существование директории, которые нам необходимы, если их нет, то создать
-if [ ! -d "/home/${USER}/data" ]; then
-        mkdir ~/data
-        mkdir ~/data/mariadb
-        mkdir ~/data/wordpress
-fi
