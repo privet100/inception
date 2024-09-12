89859fb5cb18d4c0788845f327b5ce3d418b3f55
privet100 <internet124816@gmail.com>
2024-09-12 17:11:43 -0400

    *

--- project/srcs/wordpress/wp-config.php
+++ project/srcs/wordpress/wp-config.php
@@ -1,18 +1,18 @@
 <?php
-
-if (file_exists(__DIR__ . '/.env')) {
-    $dotenv = file(__DIR__ . '/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($dotenv as $line) {
-        list($key, $value) = explode('=', $line, 2);
-        putenv(trim($key) . '=' . trim($value));
-    }
+$dotenv = file(__DIR__ . '/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+foreach ($dotenv as $line) {
+    list($key, $value) = explode('=', $line, 2);
+    putenv(trim($key) . '=' . trim($value));
+    echo "*******************************************";
+    echo $key;
+    echo $value;
 }
 
 define( 'DB_NAME',     getenv('DB_NAME') );
 define( 'DB_USER',     getenv('DB_USER') );
 define( 'DB_PASSWORD', getenv('DB_PASS') );
 define( 'DB_HOST',     'mariadb' );
-\$table_prefix = 'wp_';
+$table_prefix = 'myprefix_';
 if ( ! defined( 'ABSPATH' ) ) {
 define( 'ABSPATH', __DIR__ . '/' );}
 require_once ABSPATH . 'wp-settings.php';
