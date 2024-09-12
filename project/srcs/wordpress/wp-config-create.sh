fbf8cbb4eb258f8ac910c752e689a1ce66e59fea
privet100 <internet124816@gmail.com>
2024-09-12 17:00:56 -0400

    *

--- project/srcs/wordpress/wp-config-create.sh
+++ project/srcs/wordpress/wp-config-create.sh
@@ -5,7 +5,7 @@ define( 'DB_NAME', '${DB_NAME}' );
 define( 'DB_USER', '${DB_USER}' );
 define( 'DB_PASSWORD', '${DB_PASS}' );
 define( 'DB_HOST', 'mariadb' );
-\$table_prefix = 'wp_';
+\$table_prefix = 'myprefix_';
 if ( ! defined( 'ABSPATH' ) ) {
 define( 'ABSPATH', __DIR__ . '/' );}
 require_once ABSPATH . 'wp-settings.php';
